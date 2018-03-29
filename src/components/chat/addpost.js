ddce9597f0afe21c0fbb4a88c3e0880999dac9d2
YauheniRusinchuk <ruevgal@gmail.com>
2018-03-30 00:44:07 +0300

    Add onchange and onsubmit at input

--- src/components/chat/addpost.js
+++ src/components/chat/addpost.js
@@ -2,10 +2,36 @@ import React from 'react';
 
 
 class Addpost extends React.Component {
+
+  state = {
+    text: ''
+  }
+
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    console.log("ENTER .....");
+  }
+
+  onChange = (e) => {
+    this.setState({
+      [e.target.name] : e.target.value
+    })
+    console.log(this.state.text)
+  }
+
+
   render(){
     return(
       <div className="addpost">
-        <input type='text'  placeholder="Message"  />
+      <form onSubmit={this.onSubmit}>
+        <input
+            type="text"
+            name="text"
+            onChange={this.onChange}
+            value={this.state.text}
+            placeholder="Message"  />
+      </form>
       </div>
     );
   }
